1f1a85393ffd9094e8d74ff8b9f587526999f473
khactam94 <khac.tam.94@gmail.com>
2020-03-08 10:04:32 +0700

    chore: update layout

--- src/components/layout/AdminLinks.js
+++ src/components/layout/AdminLinks.js
@@ -2,14 +2,21 @@ import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
 
 const AdminLinks = () => {
-    return (
-        <Fragment>
-            <li className="nav-item"><Link to="/admin/dashboard" className="nav-link">Dashboard</Link></li>
-            <li className="nav-item"><Link to="/admin/users" className="nav-link">Users</Link></li>
-            <li className="nav-item"><Link to="/admin/posts" className="nav-link">Posts</Link></li>
-            <li className="nav-item"><Link to="/admin/tags" className="nav-link">Tags</Link></li>
-        </Fragment>
-    )
+  return (
+    <Fragment>
+      <li className="nav-item dropdown">
+        <a href="#" className="nav-link dropdown-toggle" id="navbarAdminLink" data-toggle="dropdown" role="button" aria-expanded="true">
+          Management <span className="caret"></span>
+        </a>
+        <div className="dropdown-menu" aria-labelledby="navbarAdminLink" role="menu">
+          <Link to="/admin/dashboard" className="nav-link">Dashboard</Link>
+          <Link to="/admin/users" className="nav-link">Users</Link>
+          <Link to="/admin/posts" className="nav-link">Posts</Link>
+          <Link to="/admin/tags" className="nav-link">Tags</Link>
+        </div>
+      </li>
+    </Fragment>
+  )
 }
 
 export default AdminLinks;
--- src/components/layout/SignedOutLinks.js
+++ src/components/layout/SignedOutLinks.js
@@ -4,8 +4,8 @@ import { NavLink } from 'react-router-dom';
 const SignedInLinks = () => {
   return (
     <ul className="nav navbar-nav navbar-right">
-      <li><NavLink to='/signup'>Sign up</NavLink></li>
-      <li><NavLink to='/login'>Login</NavLink></li>
+      <li className="nav-item"><NavLink to='/signup' className="nav-link">Sign up</NavLink></li>
+      <li className="nav-item"><NavLink to='/login' className="nav-link">Login</NavLink></li>
     </ul>
   )
 }
